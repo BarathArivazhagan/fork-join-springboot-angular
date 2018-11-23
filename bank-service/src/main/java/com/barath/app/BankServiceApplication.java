4f69f67b46158ab041c5b00bed101087ec5c503e
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-11-23 10:06:10 +0530

    Update BankServiceApplication.java

--- bank-service/src/main/java/com/barath/app/BankServiceApplication.java
+++ bank-service/src/main/java/com/barath/app/BankServiceApplication.java
@@ -3,10 +3,21 @@ package com.barath.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class BankServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankServiceApplication.class, args);
+	}
+	
+	
+	@GetMapping
+	public String ip() throws Throwable {
+		return "HOST:"+InetAddress.getLocalHost().getHostName()+", IP: "+InetAddress.getLocalHost().getHostAddress();
 	}
 }
